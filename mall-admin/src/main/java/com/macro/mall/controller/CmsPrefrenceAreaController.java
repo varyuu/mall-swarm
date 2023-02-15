6a322c839fde47638e282b91871d33a653d1c5d9
varyuu <876242846@qq.com>
2023-02-15 21:59:23 +0800

    test

--- mall-admin/src/main/java/com/macro/mall/controller/CmsPrefrenceAreaController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/CmsPrefrenceAreaController.java
@@ -28,6 +28,7 @@ public class CmsPrefrenceAreaController {
     @RequestMapping(value = "/listAll", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<CmsPrefrenceArea>> listAll() {
+        System.out.println("test");
         List<CmsPrefrenceArea> prefrenceAreaList = prefrenceAreaService.listAll();
         return CommonResult.success(prefrenceAreaList);
     }
